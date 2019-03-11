d67cbcb4a13bc781c7fc8d929604e8b154eecd13
AHorejsi <ahorejs@calstatela.edu>
2019-03-10 21:20:06 -0700

    Changed sizes for fonts of elements

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {LessDetailedAtomicElement, MoreDetailedAtomicElement} from "./ElementRepresentation";
 import ElementLegend from "./ElementLegend";
+import {NumberRow, NumberColumn} from "./NumberPositions";
 import "./Stylesheet.css";
 
-ReactDOM.render(<ElementLegend />, document.getElementById("root"));
+ReactDOM.render(<MoreDetailedAtomicElement atomicNumber="15" symbol="Hy" type="transition-metal" elementName="Hyre" atomicWeight="8.54"/>, document.getElementById("root"));
