3774452776364d4afdc05bc932faa43672aa98c4
AHorejsi <ahorejs@calstatela.edu>
2019-03-10 22:37:20 -0700

    Removed "use strict" directive from javascript files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-"use strict";
-
 import React from "react";
 import ReactDOM from "react-dom";
 import PeriodicTable from "./PeriodicTable";
 import "./Stylesheet.css";
+import "./Elements.json";
 
-window.onload = function() {
-	ReactDOM.render(<PeriodicTable />, document.getElementById("root"));
-}
+
+let xmlHttp = new XMLHttpRequest();
+
+xmlHttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		let json = this.responseText;
+		ReactDOM.render(<PeriodicTable jsonData={this.responseText} />, document.getElementById("root"));
+	}
+};
+
+xmlHttp.open("GET", "C:/Users/alexh/Desktop/periodic-table/src/Elements.json", true);
+xmlHttp.send();
