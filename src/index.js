2eb8309e3bce453688a1788d7f59cb768c1b1418
AHorejsi <ahorejs@calstatela.edu>
2019-03-10 19:27:16 -0700

    Removed unnecessary srcipt tag

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {LessDetailedAtomicElement, MoreDetailedAtomicElement} from "./ElementRepresentation";
 import "./Stylesheet.css";
+
+ReactDOM.render(<MoreDetailedAtomicElement atomicNumber="10" symbol="Yp" elementName="Yupit" atomicWeight="15.654" type="alkali" />, 
+document.getElementById("root"));
