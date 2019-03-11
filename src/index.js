cf5cf55b33910b631a56a9eb365babf9380e08b1
AHorejsi <ahorejs@calstatela.edu>
2019-03-10 19:30:50 -0700

    Added extra css class

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import ReactDOM from "react-dom";
 import {LessDetailedAtomicElement, MoreDetailedAtomicElement} from "./ElementRepresentation";
 import "./Stylesheet.css";
 
-ReactDOM.render(<MoreDetailedAtomicElement atomicNumber="10" symbol="Yp" elementName="Yupit" atomicWeight="15.654" type="alkali" />, 
+ReactDOM.render(<MoreDetailedAtomicElement atomicNumber="10" symbol="Yp" elementName="Yupit" atomicWeight="15.654" type={null} />, 
 document.getElementById("root"));
