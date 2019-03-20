47c7b368b9899cfd0d320fce4060963dd4fabfd6
AHorejsi <ahorejs@calstatela.edu>
2019-03-20 14:59:04 -0700

    Added boiling point to json file

--- src/TemperatureSlider.js
+++ src/TemperatureSlider.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const UPDATED_FROM_SLIDER = 0;
 const UPDATED_FROM_TEXTBOX = 1;
-const elemJSON = require("./ElementJSON").elementJSON;
+var elemJSON = require("./ElementJSON").elementJSON;
 
 class TemperatureSlider extends React.Component {
     constructor(props) {
@@ -11,13 +11,8 @@ class TemperatureSlider extends React.Component {
     }
 
     _updateAtomicElementStates(event, sourceOfUpdate) {
-        if (sourceOfUpdate === UPDATED_FROM_TEXTBOX) {
-            document.getElementById("temperatureSlider").value = document.getElementById("temperatureInput").value;
-        }
-        else {
-            document.getElementById("temperatureInput").value = document.getElementById("temperatureSlider").value;
-        }
-
+        let temperature = this._findTemperatureAndUpdateSlider(sourceOfUpdate);
+        
         for (elem of elemJSON) {
             let elemSymbol = document.getElementById(elem).children[1];
 
@@ -26,10 +21,41 @@ class TemperatureSlider extends React.Component {
             elemSymbol.classList.remove("solid");
             elemSymbol.classList.remove("unknown");
 
-            let phaseState; // Figure out phase state
+            let phaseState = this._computePhaseState(elem, temperature);
 
             elemSymbol.classList.add(phaseState);
         }
+    }
+
+    _findTemperatureAndUpdateSlider(sourceOfUpdate) {
+        let temperature;
+        
+        if (sourceOfUpdate === UPDATED_FROM_TEXTBOX) {
+            temperature = document.getElementById("temperatureInput").value;
+            document.getElementById("temperatureSlider").value = temperature;
+        }
+        else {
+            temperature = document.getElementById("temperatureSlider").value;
+            document.getElementById("temperatureInput").value = temperature;
+        }
+
+        return temperature;
+    }
+
+    _computePhaseState(elem, temperature) {
+        if (elem.boilingPoint !== "unknown") {
+            
+        }
+
+        if (elem.meltingPoint !== "unknown") {
+
+        }
+
+        if (elem.freezingPoint !== "unknown") {
+
+        }
+
+        return "unknown";
     }
 
     render() {
