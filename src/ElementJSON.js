90642c6248ab80efa0251cc07c709df225855739
AHorejsi <ahorejs@calstatela.edu>
2019-03-20 15:07:18 -0700

    Defined way to get json data

--- src/ElementJSON.js
+++ src/ElementJSON.js
@@ -1,1 +1,17 @@
-exports.elementJSON = null;
+"use strict";
+
+
+var elemJSON;
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+        elemJSON = JSON.parse(request.responseText);
+    }
+};
+
+request.open("GET", "./Elements.json", true);
+request.open();
+
+
+exports.elementJSON = elemJSON;
