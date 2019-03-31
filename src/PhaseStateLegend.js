4df93d4a4143d71b27cc50b3a0fbe2f742e65fee
AHorejsi <ahorejs@calstatela.edu>
2019-03-31 07:03:46 -0700

    Removed unnecessary CSS classes

--- src/PhaseStateLegend.js
+++ src/PhaseStateLegend.js
@@ -10,41 +10,41 @@ class PhaseStateLegend extends React.Component {
         return (
             <div id="phaseStateLegend" className="floatLeft">
                 <div className="phaseSymbolOuterDiv">
-                    <span id="solidLegend" className="phaseSymbol centered solidColor">
+                    <span id="solidLegend" className="phaseSymbol centered solid">
                         S
                     </span>
 
-                    <span id="solidText" className="solidColor bolded">
+                    <span id="solidText" className="solid bolded">
                         Solid
                     </span>
                 </div>
 
                 <div className="phaseSymbolOuterDiv">
-                    <span id="liquidLegend" className="phaseSymbol centered liquidColor">
+                    <span id="liquidLegend" className="phaseSymbol centered liquid">
                         L
                     </span>
 
-                    <span id="liquidText" className="liquidColor bolded">
+                    <span id="liquidText" className="liquid bolded">
                         Liquid
                     </span>
                 </div>
 
                 <div className="phaseSymbolOuterDiv">
-                    <span id="gasLegend" className="phaseSymbol centered gasColor">
+                    <span id="gasLegend" className="phaseSymbol centered gas">
                         G
                     </span>
 
-                    <span id="gasText" className="gasColor bolded">
+                    <span id="gasText" className="gas bolded">
                         Gas
                     </span>
                 </div>
 
                 <div className="phaseSymbolOuterDiv">
-                    <span id="unknownLegend" className="phaseSymbol centered unknownColor">
+                    <span id="unknownLegend" className="phaseSymbol centered unknown">
                         Uk
                     </span>
 
-                    <span id="unknownText" className="unknownColor bolded">
+                    <span id="unknownText" className="unknown bolded">
                         Unknown
                     </span>
                 </div>
