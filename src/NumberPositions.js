2ee327821ff8f21770063edc952e2faa6107cb0c
AHorejsi <ahorejs@calstatela.edu>
2019-04-02 06:36:32 -0700

    Removed slider and textbox positioning because slider stops working

--- src/NumberPositions.js
+++ src/NumberPositions.js
@@ -4,7 +4,15 @@ import React from "react";
 class NumberRow extends React.Component {
 	constructor(props) {
 		super(props);
-	}
+    }
+
+    _highlightColumn(index) {
+
+    }
+
+    _unhighlightColumn(index) {
+
+    }
 	
     render() {
         let html = [];
@@ -13,13 +21,19 @@ class NumberRow extends React.Component {
             let value;
 
             if (index === 1) {
-                value = <div className="number firstRowNumber bolded centered">{index}</div>;
+                value = <div className="number firstRowNumber bolded centered"
+                             onMouseOver={(event) => this._highlightColumn(index)}
+                             onMouseOut={(event) => this._unhighlightColumn(index)}>{index}</div>;
             } 
             else if (index < 10) {
-                value = <div className="number numberRow bolded centered">{index}</div>;
+                value = <div className="number numberRow bolded centered"
+                             onMouseOver={(event) => this._highlightColumn(index)}
+                             onMouseOut={(event) => this._unhighlightColumn(index)}>{index}</div>;
             }
             else {
-                value = <div className="number doubleDigitNumberRow bolded centered">{index}</div>;
+                value = <div className="number doubleDigitNumberRow bolded centered"
+                             onMouseOver={(event) => this._highlightColumn(index)}
+                             onMouseOut={(event) => this._unhighlightColumn(index)}>{index}</div>;
             }
 
             html.push(value);
