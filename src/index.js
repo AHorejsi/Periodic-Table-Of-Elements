1eb895d729a5b3345911a5a73f19e860971f8d83
AHorejsi <ahorejs@calstatela.edu>
2019-04-07 20:08:27 -0700

    Renamed Page to MainContent

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import $ from "jquery";
-import Page from "./Page";
+import MainContent from "./MainContent";
 import "./Stylesheet.css";
 
 
 $(document).ready(function() {
-    $.getJSON("/Elements.json", function(jsonData) {
-        ReactDOM.render(<Page json={jsonData} />, document.getElementById("main"));
+    $.getJSON("./Elements.json", function (jsonData) {
+        ReactDOM.render(<MainContent json={jsonData} />, document.getElementById("main"));
     });
 });
