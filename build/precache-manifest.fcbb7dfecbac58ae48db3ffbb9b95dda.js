08717c38fd94d85290503cf12e3ec527d5f65ef0
AHorejsi <ahorejs@calstatela.edu>
2019-04-20 03:06:33 -0700

    Adding deployable build

---
+++ build/precache-manifest.fcbb7dfecbac58ae48db3ffbb9b95dda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69527ab7f8075c68d0b8",
+    "url": "/AHorejsi/Periodic-Table-Of-Elements/static/js/runtime~main.06488ac8.js"
+  },
+  {
+    "revision": "2805bb58fc1a5736378c",
+    "url": "/AHorejsi/Periodic-Table-Of-Elements/static/js/main.965d2249.chunk.js"
+  },
+  {
+    "revision": "4a801703419dd0542b8c",
+    "url": "/AHorejsi/Periodic-Table-Of-Elements/static/js/2.01978662.chunk.js"
+  },
+  {
+    "revision": "2805bb58fc1a5736378c",
+    "url": "/AHorejsi/Periodic-Table-Of-Elements/static/css/main.ee225a44.chunk.css"
+  },
+  {
+    "revision": "08fac3e9425092f4ae9ba6e21b23517f",
+    "url": "/AHorejsi/Periodic-Table-Of-Elements/index.html"
+  }
+];
